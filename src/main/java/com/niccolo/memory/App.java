9fcaccb82b44790626607bebb0c2cb151d457c41
Mangialavori-Niccolo <niccolomangialavori@gmail.com>
2025-06-18 22:00:19 +0200

    Beginning of the levels implementation

--- src/main/java/com/niccolo/memory/App.java
+++ src/main/java/com/niccolo/memory/App.java
@@ -5,18 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class App extends Application {
-    /*@Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/niccolo/memory/MemoryGame.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Memory Game");
-        stage.setScene(scene);
-        stage.show();
-    }
-*/
     @Override
     public void start(Stage stage) {
         try {
---
+++ src/main/java/com/niccolo/memory/controller/StartingMenuController.java
@@ -0,0 +1,4 @@
+package com.niccolo.memory.controller;
+
+public class StartingMenuController {
+}
